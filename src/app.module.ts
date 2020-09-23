ee58fac90437f45b2283ef108cb9bcb70298c985
Aviv Ronen <info@arps.co.il>
2020-09-23 17:50:02 +0300

    Auth signup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,19 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
+//import { AuthController } from './auth/auth.controller';
+//import { AuthService } from './auth/auth.service';
+import { AuthModule } from './auth/auth.module';
+//import { UserRepository } from './auth/user.repository';
+
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
-    TasksModule
+    TasksModule,
+    AuthModule
   ],
+  //controllers: [AuthController],
+  //providers: [AuthService],
 })
 export class AppModule {}
